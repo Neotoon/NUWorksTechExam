3068d00317a1414350bbb0074d6a5c91773bc938
Neotoon <61035850+Neotoon@users.noreply.github.com>
2023-04-19 18:24:59 +0800

    added delete button

--- CRUD/backend/server.js
+++ CRUD/backend/server.js
@@ -45,6 +45,15 @@ app.put("/update/:id",(req,res)=>{
     })
 })
 
+app.delete('/todo/:id',(req,res)=>{
+    const sql="DELETE FROM todo WHERE ID = ?";
+    const id=req.params.id;
+    db.query(sql,[id],(err,data)=>{
+        if(err) return res.json("post");
+        return res.json(data);
+    })
+})
+
 app.listen(8081,()=>{
     console.log("listening");
 })
--- CRUD/frontend/src/todo.js
+++ CRUD/frontend/src/todo.js
@@ -10,6 +10,15 @@ function Todo(){
         .catch(err=>console.log(err))
     },[])
 
+    const handleDelete=async(id)=>{
+        try{
+            await axios.delete('http://localhost:8081/todo/'+id)
+            window.location.reload()
+        }catch(err){
+            console.log(err);
+        }
+    }
+
     return(
         <div className='d-flex vh-100 bg-white justify-content-center align-items-center'>
             <div className='w-50 rounded p-3'>
@@ -28,7 +37,7 @@ function Todo(){
                                     <td>{data.Todo}</td>
                                     <td>
                                         <Link to={`update/${data.ID}`} className='btn btn-primary'>Update</Link>
-                                        <button className='btn btn-danger ms-2'>Done</button>
+                                        <button className='btn btn-danger ms-2' onClick={e=>handleDelete(data.ID)}>Done</button>
                                     </td>
                                 </tr>
                             ))
